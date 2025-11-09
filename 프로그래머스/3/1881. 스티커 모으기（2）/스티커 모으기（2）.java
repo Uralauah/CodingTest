6ce75a6824bba8eb7a1cf92563e2483263924599
이승현 <50231309+Uralauah@users.noreply.github.com>
2025-11-09 23:37:48 +0900

    [level 3] Title: 스티커 모으기(2), Time: 6.59 ms, Memory: 57.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/1881. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int solution(int sticker[]) {
+        int answer = 0;
+        int n = sticker.length;
+        
+        if(n<=3){
+            for(int i=0;i<n;i++){
+                answer = Math.max(answer , sticker[i]);
+            }
+            return answer;
+        }
+        
+        int[] dp = new int[n];
+        dp[1] = sticker[1];
+        for(int i=2;i<n;i++){
+            dp[i] = Math.max(dp[i-1], dp[i-2]+sticker[i]);
+        }
+        
+        answer = dp[n-1];
+        
+        dp[0] = sticker[0];
+        dp[1] = sticker[0];
+        for(int i=2;i<n-1;i++){
+            dp[i] = Math.max(dp[i-1] , dp[i-2]+sticker[i]);
+        }
+        
+        answer = Math.max(answer , dp[n-2]);
+
+        return answer;
+    }
+}
