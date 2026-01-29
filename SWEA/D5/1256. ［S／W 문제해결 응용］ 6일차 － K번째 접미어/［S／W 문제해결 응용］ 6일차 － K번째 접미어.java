078e52cd0c9f757e690a93c9b0095d5c8902e106
이승현 <50231309+Uralauah@users.noreply.github.com>
2026-01-29 09:53:54 +0900

    [D5] Title: [S/W 문제해결 응용] 6일차 - K번째 접미어, Time: 99 ms, Memory: 26,752 KB -BaekjoonHub

---
+++ SWEA/D5/1256. ［S／W 문제해결 응용］ 6일차 － K번째 접미어/［S／W 문제해결 응용］ 6일차 － K번째 접미어.java
@@ -0,0 +1,61 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.util.*;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			int k = sc.nextInt();
+			String input = sc.next();
+			
+			PriorityQueue<String> pq = new PriorityQueue<>();
+			for(int i=0;i<input.length();i++) {
+				pq.add(input.substring(i));
+			}
+			
+			if(k>input.length()) {
+				System.out.println("none");
+				continue;
+			}
+			
+			for(int i=0;i<k-1;i++) {
+				pq.poll();
+			}
+			System.out.println("#"+test_case+" "+pq.peek());
+		}
+	}
+}
