afee8710cca5a46a1dcb73e8a634a9e964a14f2c
이승현 <50231309+Uralauah@users.noreply.github.com>
2026-01-29 09:55:00 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 1,448 ms, Memory: 28,416 KB -BaekjoonHub

---
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
@@ -0,0 +1,108 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+    public static boolean[] visited;
+    public static int[] home;
+
+    public static int ans, n, lx, ly;
+
+    public static void find(int x, int y, int[][] cust, int sum){
+        boolean flag = false;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                flag = true;
+                int now = Math.abs(x-cust[i][0])+Math.abs(y-cust[i][1]);
+                visited[i] = true;
+                find(cust[i][0], cust[i][1], cust, sum+now);
+                visited[i] = false;
+            }
+        }
+        if(!flag){
+            sum += Math.abs(x - home[0]) + Math.abs(y - home[1]);
+            if (ans > sum) {
+                ans = sum;
+                lx = x;
+                ly = y;
+            }
+        }
+    }
+	public static void main(String args[]) throws Exception
+	{
+		/*
+		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
+		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
+		 */
+		//System.setIn(new FileInputStream("res/input.txt"));
+
+		/*
+		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
+		 */
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		/*
+		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		*/
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			n = sc.nextInt();
+
+            int[] comp = new int[2];
+            home = new int[2];
+            visited = new boolean[n];
+            ans = Integer.MAX_VALUE;
+
+            int[][] cust = new int[n][2];
+            comp[0] = sc.nextInt();
+            comp[1] = sc.nextInt();
+            home[0] = sc.nextInt();
+            home[1] = sc.nextInt();
+
+            for(int i=0;i<n;i++){
+                cust[i][0] = sc.nextInt();
+                cust[i][1] = sc.nextInt();
+            }
+
+            find(comp[0], comp[1], cust, 0);
+            System.out.println("#"+test_case+" "+ans);
+        }
+	}
+}
