ac1fc8a3bac5c53a78a301abd4ff1db87d5b9078
이승현 <50231309+Uralauah@users.noreply.github.com>
2026-02-02 09:55:23 +0900

    [Gold II] Title: 트리, Time: 416 ms, Memory: 46472 KB -BaekjoonHub

--- 백준/Gold/4256. 트리/트리.java
+++ 백준/Gold/4256. 트리/트리.java
@@ -2,51 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static int[] pre, in;
-	public static int n;
+	public static int[] pre;
+	public static int n, preIndex;
 	public static StringBuilder sb;
+	public static Map<Integer, Integer> in;
 
-	public static class Node {
-		int num;
-		Node left;
-		Node right;
+	public static void find(int si, int ei) {
+		if (si > ei)
+			return;
 
-		public Node(int num) {
-			this.num = num;
-		}
-	}
+		int rootVal = pre[preIndex++];
+		int rootIdx = in.get(rootVal);
 
-	public static Node find(int si, int ei) {
-		if (si >= ei) {
-			return null;
-		}
-		Map<Integer, Integer> s = new HashMap<>();
-		for (int i = si; i < ei; i++) {
-			s.put(in[i], i);
-		}
+		find(si, rootIdx - 1);
+		find(rootIdx + 1, ei);
 
-		int p = -1;
-		for (int i = 0; i < n; i++) {
-			if (s.containsKey(pre[i])) {
-				p = pre[i];
-				break;
-			}
-		}
-		Node root = new Node(p);
-
-		root.left = find(si, s.get(p));
-		root.right = find(s.get(p) + 1, ei);
-		return root;
-	}
-	
-	public static void post(Node root) {
-		if(root.left!=null) {
-			post(root.left);
-		}
-		if(root.right!=null) {
-			post(root.right);
-		}
-		sb.append(root.num).append(" ");
+		sb.append(rootVal).append(" ");
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -56,28 +27,24 @@ public class Main {
 		int T = Integer.parseInt(br.readLine());
 
 		for (int t = 0; t < T; t++) {
+			in = new HashMap<>();
+			preIndex = 0;
 			n = Integer.parseInt(br.readLine());
 
 			pre = new int[n];
-			in = new int[n];
 
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < n; i++) {
 				pre[i] = Integer.parseInt(st.nextToken());
 			}
 
-			int mid = -1;
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < n; i++) {
-				in[i] = Integer.parseInt(st.nextToken());
-				if (in[i] == pre[0]) {
-					mid = i;
-				}
+				in.put(Integer.parseInt(st.nextToken()), i);
 			}
 
-			Node root = find(0,n);
 			sb = new StringBuilder();
-			post(root);
+			find(0, n - 1);
 			System.out.println(sb.toString());
 		}
 	}
