03d72c8afbf388764572eae4188985ab0b041e8e
이승현 <50231309+Uralauah@users.noreply.github.com>
2026-05-23 15:48:28 +0900

    [Passed] 서로 다른 BST 개수 세기 (123ms, 10MB)

---
+++ 코드트리/trail4/서로 다른 BST 개수 세기/number-of-unique-bst.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        long[] dp = new long[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int root = 1; root <= i; root++) {
+                dp[i] += dp[root - 1] * dp[i - root];
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
