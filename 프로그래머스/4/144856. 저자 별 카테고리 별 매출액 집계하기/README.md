8998d20303e87a1fd8e7817ec63a43a58a44d928
이승현 <50231309+Uralauah@users.noreply.github.com>
2026-06-26 13:03:51 +0900

    [level 4] Title: 저자 별 카테고리 별 매출액 집계하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/README.md
+++ 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/README.md
@@ -1,10 +1,10 @@
 # [level 4] 저자 별 카테고리 별 매출액 집계하기 - 144856 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/144856) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/144856?language=mysql) 
 
 ### 성능 요약
 
-메모리: 0.0 MB, 시간: 0.00 ms
+메모리: undefined, 시간: 
 
 ### 구분
 
@@ -12,11 +12,11 @@
 
 ### 채점결과
 
-Empty
+합계: 100.0 / 100.0
 
 ### 제출 일자
 
-2025년 02월 21일 13:16:08
+2026년 06월 26일 13:03:47
 
 ### 문제 설명
 
--- 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
+++ 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
@@ -1,8 +1,20 @@
--- 코드를 입력하세요
-SELECT a.author_id, a.author_name, b.category, sum(b.price*bs.sales) as total_sales
+with books as (
+    select 
+        b.category as category, 
+        (b.price * bs.sales) as price,
+        b.author_id as author_id
+    from book b
+    join book_sales bs
+    on b.book_id = bs.book_id
+    where bs.sales_date >= '2022-01-01' and bs.sales_date < '2022-02-01'
+)
+SELECT 
+    a.author_id as author_id,
+    a.author_name as author_name,
+    b.category as category,
+    sum(b.price) as total_sales
 from author a
-join book b on a.author_id = b.author_id
-join book_sales bs on b.book_id = bs.book_id
-where to_char(bs.sales_date,'mm') = '01'
-group by a.author_id, a.author_name, b.category
-order by author_id asc, category desc;
+join books b
+on a.author_id = b.author_id
+group by author_id, category
+order by author_id asc, category desc
