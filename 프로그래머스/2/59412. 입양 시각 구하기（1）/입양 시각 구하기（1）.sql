4c8017ae07eab85c21ff987c038904c40d447876
이승현 <50231309+Uralauah@users.noreply.github.com>
2026-06-26 10:54:27 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -1,6 +1,13 @@
 -- 코드를 입력하세요
-SELECT hour(datetime) as hour, count(*) as count
-from animal_outs
-group by hour(datetime)
-having hour >=9 and hour <=19
-order by hour asc
+with recursive hour as(
+    select 9 as h
+    union all
+    select h + 1 from hour
+    where h < 19
+)
+select hr.h, count(ao.animal_id) as count
+from hour hr
+left join animal_outs ao
+on hr.h = hour(ao.datetime)
+group by hr.h
+order by hr.h
