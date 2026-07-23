c900bc6b6c29c92914866cfb3bfbe4e0370bd81e
이승현 <50231309+Uralauah@users.noreply.github.com>
2026-07-23 18:02:26 +0900

    [level 2] Title: [3차] 압축, Time: 3.29 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -2,29 +2,31 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String msg) {
-        int[] answer = {};
-        HashMap<String,Integer> hm = new HashMap<>();
-        ArrayList<Integer> ans = new ArrayList<>();
+        
+        Map<String, Integer> map = new HashMap<>();
+        
         for(int i=0;i<26;i++){
-            hm.put(String.valueOf((char)('A'+i)),i+1);
+            map.put(String.valueOf((char)('A'+i)), i+1);
         }
         int len = 27;
+        
+        List<Integer> ans = new ArrayList<>();
         
         int i=0;
         while(i<msg.length()){
             int idx = i+1;
-            while(idx<=msg.length()&&hm.containsKey(msg.substring(i,idx))){
+            while(idx<=msg.length()&&map.containsKey(msg.substring(i,idx))){
                 idx++;
             }
-            ans.add(hm.get(msg.substring(i,idx-1)));
+            ans.add(map.get(msg.substring(i,idx-1)));
             
             if(idx<=msg.length())
-                hm.put(msg.substring(i,idx),len++);
+                map.put(msg.substring(i,idx),len++);
             
             i = idx-1;
         }
         
-        answer = new int[ans.size()];
+        int[] answer = new int[ans.size()];
         for(i=0;i<ans.size();i++){
             answer[i] = ans.get(i);
         }
