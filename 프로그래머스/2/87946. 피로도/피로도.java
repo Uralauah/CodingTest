58d9231895953af1707b7e19cf1ee2b67533d2c0
이승현 <50231309+Uralauah@users.noreply.github.com>
2026-07-23 20:13:14 +0900

    [level 2] Title: 피로도, Time: 1.55 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,36 +1,33 @@
 class Solution {
+    public int answer = 0, k, n;
     public int[][] dungeons;
     public boolean[] visited;
-    public int n, answer;
     
-    public void find(int remain, int idx, int sum){
-        remain -= dungeons[idx][1];
+    public void find(int remain, int sum){
         boolean flag = false;
+        
         for(int i=0;i<n;i++){
-            if(!visited[i] && dungeons[i][0]<=remain){
+            if(!visited[i] && dungeons[i][0] <= remain){
                 visited[i] = true;
-                find(remain, i, sum+1);
                 flag = true;
+                find(remain-dungeons[i][1], sum+1);
                 visited[i] = false;
             }
         }
+        
         if(!flag){
             answer = Math.max(answer, sum);
         }
     }
     
     public int solution(int k, int[][] dungeons) {
-        answer = -1;
-        n = dungeons.length;
+        this.k = k;
+        this.n = dungeons.length;
         this.dungeons = dungeons;
+        
         visited = new boolean[n];
-        for(int i=0;i<n;i++){
-            if(k>=dungeons[i][0]){
-                visited[i] = true;
-                find(k, i, 1);
-                visited[i] = false;
-            }
-        }
+        find(k,0);
+        
         return answer;
     }
 }
