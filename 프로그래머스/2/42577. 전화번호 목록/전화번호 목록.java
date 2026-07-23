d7f073dfad64cd3d6855e527f83487097e8b9f2f
이승현 <50231309+Uralauah@users.noreply.github.com>
2026-07-23 12:14:35 +0900

    [level 2] Title: 전화번호 목록, Time: 302.90 ms, Memory: 237 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -15,19 +15,5 @@ class Solution {
         }
         
         return answer;
-//         boolean answer = true;
-//         HashSet<String> hs = new HashSet<>();
-//         for(int i=0;i<phone_book.length;i++){
-//             hs.add(phone_book[i]);
-//         }
-        
-//         for(int i=0;i<phone_book.length;i++){
-//             for(int j=1;j<phone_book[i].length();j++){
-//                 if(hs.contains(phone_book[i].substring(0,j))){
-//                     return false;
-//                 }
-//             }
-//         }
-//         return answer;
     }
 }
