28bfc0119733973eb97053f86e3129a14ea15eef
이승현 <50231309+Uralauah@users.noreply.github.com>
2026-07-24 13:07:20 +0900

    [level 3] Title: N으로 표현, Time: 12.20 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -2,52 +2,39 @@ import java.util.*;
 
 class Solution {
     public int solution(int N, int number) {
-        int answer = 0;
-        
-        if(N==number)
-            return 1;
+        int answer = -1;
         
         Set<Integer>[] dp = new HashSet[9];
-        for(int i=1;i<9;i++){
+        int t = N;
+        for(int i=1;i<=8;i++){
             dp[i] = new HashSet<>();
+            dp[i].add(t);
+            if(t == number)
+                return i;
+            t = t*10 + N;
         }
         
         dp[1].add(N);
         
         for(int i=2;i<=8;i++){
-            int num = 0;
-            for(int a=0;a<i;a++){
-                num=num*10+N;
-            }
-            dp[i].add(num);
-            
             for(int j=1;j<i;j++){
-                int[] arr1 = dp[j].stream().mapToInt(Integer::intValue).toArray();
-                int[] arr2 = dp[i-j].stream().mapToInt(Integer::intValue).toArray();
+                int k = i-j;
                 
-                for(int k=0;k<arr1.length;k++){
-                    for(int l=0;l<arr2.length;l++){
-                        dp[i].add(arr1[k]+arr2[l]);
-                        dp[i].add(arr1[k]-arr2[l]);
-                        dp[i].add(arr1[k]*arr2[l]);
-                        dp[i].add(arr1[k]/arr2[l]);
-                        dp[i].add(arr2[l]-arr1[k]);
-                        dp[i].add(arr2[l]/arr1[k]);
+                for(int a : dp[j]){
+                    for(int b : dp[k]){
+                        dp[i].add(a+b);
+                        dp[i].add(a*b);
+                        dp[i].add(a-b);
+                        if(b!=0)
+                            dp[i].add(a/b);
+                        
+                        if(dp[i].contains(number)){
+                            return i;
+                        }
                     }
                 }
             }
-            if(dp[i].contains(number)){
-                answer = i;
-                break;
-            }
-            dp[i].remove(0);
         }
-        
-        return answer==0?-1:answer;
+        return answer;
     }
 }
-
-/*
-dp[] 배열 : dp[i] = i개의 수를 가지고 만들 수 있는 수들 리스트
-dp[i] = dp[j] 사칙연산 dp[i-j]  1<=j<i
-*/
--- 프로그래머스/3/42895. N으로 표현/README.md
+++ 프로그래머스/3/42895. N으로 표현/README.md
@@ -1,10 +1,10 @@
 # [level 3] N으로 표현 - 42895 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895?utm_source=chatgpt.com) 
 
 ### 성능 요약
 
-메모리: 82.2 MB, 시간: 28.04 ms
+메모리: 89.5 MB, 시간: 12.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 07일 13:30:47
+2026년 07월 24일 13:07:16
 
 ### 문제 설명
 
