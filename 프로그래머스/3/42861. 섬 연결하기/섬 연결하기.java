df0bf18410c51dadbec8f1a2a200167578e5b851
이승현 <50231309+Uralauah@users.noreply.github.com>
2026-07-24 20:57:43 +0900

    [level 3] Title: 섬 연결하기, Time: 2.26 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,58 +1,48 @@
 import java.util.*;
 
 class Solution {
-    public class Node{
-        int num;
-        int len;
+    public int[] parent;
+    
+    public int find(int a){
+        if(parent[a] == a)
+            return a;
         
-        public Node(int num, int len){
-            this.num = num;
-            this.len = len;
-        }
+        return parent[a] = find(parent[a]);
     }
     
-    public int[][] arr;
+    public void union(int a, int b){
+        a = find(a);
+        b = find(b);
+        
+        if(a!=b){
+            parent[b] = a;
+        }
+    }
     
     public int solution(int n, int[][] costs) {
         int answer = 0;
         
-        arr = new int[n][n];
-        
-        for(int i=0;i<costs.length;i++){
-            arr[costs[i][0]][costs[i][1]] = costs[i][2];
-            arr[costs[i][1]][costs[i][0]] = costs[i][2];
-        }
-        
-        PriorityQueue<Node> pq = new PriorityQueue<>(new Comparator<Node>(){
-            @Override
-            public int compare(Node o1, Node o2){
-                return o1.len - o2.len;
-            }
-        });
-        
+        parent = new int[n];
         for(int i=0;i<n;i++){
-            if(arr[0][i]==0)
-                continue;
-            pq.add(new Node(i, arr[0][i]));
+            parent[i] = i;
         }
         
-        Set<Integer> set = new HashSet<>();
-    
-        set.add(0);
+        Arrays.sort(costs, (a,b)-> Integer.compare(a[2], b[2]));
         
-        while(!pq.isEmpty()){
-            Node now = pq.poll();
+        int cnt = 0;
+        for(int i=0;i<costs.length;i++){
+            int a = costs[i][0];
+            int b = costs[i][1];
             
-            if(set.contains(now.num))
+            if(find(a) == find(b))
                 continue;
-            set.add(now.num);
-            answer += now.len;
             
-            for(int i=0;i<n;i++){
-                if(arr[now.num][i]==0)
-                    continue;
-                pq.add(new Node(i, arr[now.num][i]));
-            }
+            union(a,b);
+            cnt++;
+            answer += costs[i][2];
+            
+            if(cnt == n-1)
+                break;
         }
         
         return answer;
