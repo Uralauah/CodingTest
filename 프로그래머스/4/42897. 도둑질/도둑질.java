6e14c86a385c3685fce57707c012a8abb58b19c5
이승현 <50231309+Uralauah@users.noreply.github.com>
2026-07-24 16:15:53 +0900

    [level 4] Title: 도둑질, Time: 15.55 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -1,55 +1,20 @@
 class Solution {
     public int solution(int[] money) {
-        int len = money.length;
+        int answer = 0;
+        int n = money.length;
+        int[][] dp = new int[2][n];
         
-        return Math.max(rob(0,len-2, money), rob(1,len-1,money));
-    }
-    
-    public int rob(int s, int e, int[] money){
-        int prev2 = 0;
-        int prev1 = 0;
-        // System.out.println(s+" asdf "+e);
-        for(int i=s;i<=e;i++){
-            int steal = prev2 + money[i];
-            int notSteal = prev1;
-            int cur = Math.max(steal, notSteal);
-            prev2 = prev1;
-            prev1 = cur;
-            
-            
-            // System.out.println(prev1+" "+prev2);
+        dp[0][0] = money[0];
+        dp[0][1] = Math.max(money[0], money[1]);
+        dp[1][1] = money[1];
+        
+        
+        for(int i=2;i<n;i++){
+            if(i!=n-1)
+                dp[0][i] = Math.max(dp[0][i-1], dp[0][i-2] + money[i]);
+            dp[1][i] = Math.max(dp[1][i-1], dp[1][i-2] + money[i]);
         }
-        return prev1;
+        
+        return Math.max(dp[0][n - 2], dp[1][n - 1]);
     }
-//     public int solution(int[] money) {
-//         int len = money.length;
-//         int[][] dpA = new int[len][2];
-//         int[][] dpB = new int[len][2];
-        
-//         dpA[0][0] = money[0];
-//         for(int i=1;i<len;i++){
-//             dpB[i][0] = dpB[i-1][1]+money[i];
-//             dpB[i][1] = Math.max(dpB[i-1][0], dpB[i-1][1]);
-            
-//             if(i==len-1)
-//                 break;
-//             dpA[i][0] = dpA[i-1][1]+money[i];
-//             dpA[i][1] = Math.max(dpA[i-1][0], dpA[i-1][1]);
-            
-//         }
-        
-//         int a = Math.max(dpA[len-2][0], dpA[len-2][1]);
-        
-//         int b = Math.max(dpB[len-1][0], dpB[len-1][1]);
-        
-//         return Math.max(a,b);
-//     }
 }
-/*
-2차원 배열 dp
-[n][0] 해당 집에서 돈 훔쳤을 때 최댓값
-[n][1] 해당 집에서 돈 훔치지 않았을 때 최댓값
-
-돈을 훔쳤으면 [n][0] = Math.max([n-1][1]+money[n], [n][0])
-안훔쳤으면 [n][1] = Math.max([n-1][0], [n-1][1])
-*/
