bede00cdcd5fa4c6f2b87ff5cdd29e9ed705dbfa
이승현 <50231309+Uralauah@users.noreply.github.com>
2026-07-25 11:10:39 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.73 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,50 +1,49 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
+    public int[][] dir = {{1,0},{0,-1},{0,1},{-1,0}};
+    public String[] op = {"d","l","r","u"};
+    
+    public int dist(int x1, int y1, int x2, int y2){
+        return Math.abs(x1-x2) + Math.abs(y1 - y2);
+    }
+    
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        StringBuilder answer = new StringBuilder();
-        int down = 0, left = 0, up = 0, right = 0;
+        int distance = dist(x,y,r,c);
         
-        int diff = Math.abs(x-r) + Math.abs(y-c);
-        k -= diff;
-        
-        if(k<0 || k%2!=0){
+        if(distance > k)
             return "impossible";
+        
+        StringBuilder ans = new StringBuilder();
+        
+        int curX = x;
+        int curY = y;
+        
+        for(int i=0;i<k;i++){
+            boolean moved = false;
+            
+            for(int d=0;d<4;d++){
+                int nextX = curX + dir[d][0];
+                int nextY = curY + dir[d][1];
+                
+                if(nextX<1 || nextX>n || nextY<1 || nextY>m)
+                    continue;
+                
+                int nextDist = dist(nextX, nextY, r, c);
+                
+                if(nextDist <= k-i-1){
+                    ans.append(op[d]);
+                    curX = nextX;
+                    curY = nextY;
+                    moved = true;
+                    break;
+                }
+            }
+            
+            if(!moved)
+                return "impossible";
         }
         
-        if(x>r){
-            up+=x-r;
-        }
-        else{
-            down+=r-x;
-        }
-        
-        if(y>c){
-            left += y-c;
-        }
-        else{
-            right += c-y;
-        }
-        
-        answer.append("d".repeat(down));
-        int extra_down = Math.min(k/2,n-(x+down));
-        answer.append("d".repeat(extra_down));
-        up += extra_down;
-        k-=2*extra_down;
-        
-        answer.append("l".repeat(left));
-        int extra_left = Math.min(k/2,y-left-1);
-        answer.append("l".repeat(extra_left));
-        right += extra_left;
-        k-=2*extra_left;
-        
-        answer.append("rl".repeat(k/2));
-        
-        answer.append("r".repeat(right));
-        answer.append("u".repeat(up));
-        
-        
-        return answer.toString();
+        return ans.toString();
     }
 }
